57363e3368406221381641077186f89ef677721f
Felix <felixyeu25@gmail.com>
2021-12-16 11:50:25 +0100

    petit patch couche DAL

--- Depot.DAL/Member_DAL.cs
+++ Depot.DAL/Member_DAL.cs
@@ -12,13 +12,15 @@ namespace Raminagrobis.DAL
         public string Email { get; private set; }
         public string Address { get; private set; }
 
+        public DateTime Create_at { get; private set; }
+
         public int ID { get; set; }
 
-        public Member_DAL(string company, string civility, string surname, string name, string email, string address) 
-                => (Company, Civility, Surname, Name, Email, Address) = (company, civility, surname, name, email, address);
+        public Member_DAL(string company, string civility, string surname, string name, string email, string address, DateTime create_at) 
+                => (Company, Civility, Surname, Name, Email, Address, Create_at) = (company, civility, surname, name, email, address, create_at);
 
-        public Member_DAL(int id, string company, string civility, string surname, string name, string email, string address)
-                => (ID, Company, Civility, Surname, Name, Email, Address) = (id, company, civility, surname, name, email, address);
+        public Member_DAL(int id, string company, string civility, string surname, string name, string email, string address, DateTime create_at)
+                => (ID, Company, Civility, Surname, Name, Email, Address , Create_at) = (id, company, civility, surname, name, email, address, create_at);
 
         internal void Insert(SqlConnection connexion)
         {
@@ -34,6 +36,7 @@ namespace Raminagrobis.DAL
                 commande.Parameters.Add(new SqlParameter("@name", Name));
                 commande.Parameters.Add(new SqlParameter("@email", Email));
                 commande.Parameters.Add(new SqlParameter("@address", Address));
+                commande.Parameters.Add(new SqlParameter("@create_at", Create_at));
                 commande.Parameters.Add(new SqlParameter("@id", ID));
 
                 commande.ExecuteNonQuery();
--- Depot.DAL/Supplier_DAL.cs
+++ Depot.DAL/Supplier_DAL.cs
@@ -11,22 +11,23 @@ namespace Raminagrobis.DAL
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Address { get; private set; }
+        public DateTime Create_at { get; private set; }  
 
         public int ID { get; set; }
 
-        public Supplier_DAL(string company, string civility, string surname, string name, string email, string address) 
-                => (Company, Civility, Surname, Name, Email, Address) = (company, civility, surname, name, email, address);
+        public Supplier_DAL(string company, string civility, string surname, string name, string email, string address, DateTime create_at) 
+                => (Company, Civility, Surname, Name, Email, Address, Create_at) = (company, civility, surname, name, email, address, create_at);
 
-        public Supplier_DAL(int id, string company, string civility, string surname, string name, string email, string address)
-                => (ID, Company, Civility, Surname, Name, Email, Address) = (id, company, civility, surname, name, email, address);
+        public Supplier_DAL(int id, string company, string civility, string surname, string name, string email, string address, DateTime create_at)
+                => (ID, Company, Civility, Surname, Name, Email, Address, Create_at) = (id, company, civility, surname, name, email, address, create_at);
 
         internal void Insert(SqlConnection connexion)
         {
             using (var commande = new SqlCommand())
             {
                 commande.Connection = connexion;
-                commande.CommandText = "insert into supplier(company,civility,surname,name,email,address,id)"
-                                + " values (@company, @civility, @surname, @name, @email, @address, @id )";
+                commande.CommandText = "insert into supplier(company,civility,surname,name,email,address,create_at,id)"
+                                + " values (@company, @civility, @surname, @name, @email, @address, @create_at, @id )";
 
                 commande.Parameters.Add(new SqlParameter("@company", Company));
                 commande.Parameters.Add(new SqlParameter("@civility", Civility));
@@ -34,6 +35,7 @@ namespace Raminagrobis.DAL
                 commande.Parameters.Add(new SqlParameter("@name", Name));
                 commande.Parameters.Add(new SqlParameter("@email", Email));
                 commande.Parameters.Add(new SqlParameter("@address", Address));
+                commande.Parameters.Add(new SqlParameter("@create_at", Create_at));
                 commande.Parameters.Add(new SqlParameter("@id", ID));
 
                 commande.ExecuteNonQuery();
