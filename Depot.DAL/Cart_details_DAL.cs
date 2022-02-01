1f5e3d1bfdb8b531d10cc83f3cd7e3045c1509a7
felix bernard <Bambouzelle>
2022-02-01 16:35:33 +0100

    patch

--- Depot.DAL/Cart_details_DAL.cs
+++ Depot.DAL/Cart_details_DAL.cs
@@ -17,22 +17,6 @@ namespace Raminagrobis.DAL
         public Cart_details_DAL(int id,int id_cart, int id_references, int id_global_details, int quantity)
                 => (ID, Id_cart, Id_references, Id_global_details, Quantity) = (id, id_cart, id_references, id_global_details, quantity);
 
-        internal void Insert(SqlConnection connexion)
-        {
-            using (var commande = new SqlCommand())
-            {
-                commande.Connection = connexion;
-                commande.CommandText = "insert into supplier(id_cart,id_references,id_global_details,quantity,id)"
-                                + " values (@id_cart, @id_references, @id_global_details, @quantity, @id )";
-
-                commande.Parameters.Add(new SqlParameter("@id_cart", Id_cart));
-                commande.Parameters.Add(new SqlParameter("@id_references", Id_references));
-                commande.Parameters.Add(new SqlParameter("@id_global_details", Id_global_details));
-                commande.Parameters.Add(new SqlParameter("@quantity", Quantity));
-                commande.Parameters.Add(new SqlParameter("@id", ID));
-
-                commande.ExecuteNonQuery();
-            }
         }
     }
 }
--- Depot.DAL/Price_DAL.cs
+++ Depot.DAL/Price_DAL.cs
@@ -16,22 +16,5 @@ namespace Raminagrobis.DAL
 
         public Price_DAL(int id, int id_global_details, int id_supplier, float price)
          => (ID, Id_global_details, Id_supplier, Price) = (id, id_global_details, id_supplier, price);
-
-        internal void Insert(SqlConnection connexion)
-        {
-            using (var commande = new SqlCommand())
-            {
-                commande.Connection = connexion;
-                commande.CommandText = "insert into supplier(id_global_details,id_supplier,price,id)"
-                                + " values (@id_global_details, @id_supplier, @price, @id )";
-
-                commande.Parameters.Add(new SqlParameter("@id_fournisseur", Id_global_details));
-                commande.Parameters.Add(new SqlParameter("@id_references", Id_supplier));
-                commande.Parameters.Add(new SqlParameter("@price",Price));
-                commande.Parameters.Add(new SqlParameter("@id", ID));
-
-                commande.ExecuteNonQuery();
-            }
-        }
     }
 }
